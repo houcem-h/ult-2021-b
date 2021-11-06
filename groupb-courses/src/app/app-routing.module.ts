59ea5fcddd4f74dbcf8a34cf4156a38b621c29a3
Houcem Hedhly <hedhoucem@gmail.com>
2021-11-06 09:59:25 +0100

    redirect to home

--- groupb-courses/src/app/app-routing.module.ts
+++ groupb-courses/src/app/app-routing.module.ts
@@ -6,9 +6,10 @@ import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent }
 ];
 
