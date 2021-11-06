8345edf2d495ddbdc818bb014ddb489f60bee883
Houcem Hedhly <hedhoucem@gmail.com>
2021-11-06 08:27:11 +0100

    create routes

--- groupb-courses/src/app/app-routing.module.ts
+++ groupb-courses/src/app/app-routing.module.ts
@@ -3,7 +3,10 @@ import { RouterModule, Routes } from "@angular/router";
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent }
+];
 
 @NgModule({
   declarations: [
