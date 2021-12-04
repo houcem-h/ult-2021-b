fa31c0a2530d5f506541c53a2dfed3ec82a8fb16
Houcem Hedhly <hedhoucem@gmail.com>
2021-12-04 08:49:30 +0100

    create a route for course-details component

--- groupb-courses/src/app/courses/courses-routing.module.ts
+++ groupb-courses/src/app/courses/courses-routing.module.ts
@@ -3,7 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { CoursesComponent } from './courses.component';
 import { CoursesDetailsComponent } from './courses-details/courses-details.component';
 
-const routes: Routes = [{ path: '', component: CoursesComponent }];
+const routes: Routes = [
+  { path: '', component: CoursesComponent },
+  { path: ':id', component: CoursesDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
