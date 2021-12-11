a35172b25c4eb0b1df3564bd7ae920bbc9d5db82
Houcem Hedhly <hedhoucem@gmail.com>
2021-12-11 08:55:21 +0100

    get cart content from localstorage

--- groupb-courses/src/app/cart/cart.component.ts
+++ groupb-courses/src/app/cart/cart.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CartService } from "src/app/services/cart.service";
+
+import { courses } from "src/app/courses-list";
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  cartContent: any = [];
 
-  constructor() { }
+  constructor(
+    private cartService: CartService
+  ) { }
 
   ngOnInit(): void {
+    this.cartContent = this.cartService.cartContent;
+    for (let index = 0; index < this.cartContent.length; index++) {
+     const course = courses.filter(course => course.id == this.cartContent[index].id)[0]
+     this.cartContent[index].title = course.title;
+     this.cartContent[index].price = course.price;
+    }
+    console.log(this.cartContent);
+
+
   }
 
 }
