2e303380c62ec0fbc741115ae37fbdce6083dd57
Houcem Hedhly <hedhoucem@gmail.com>
2021-12-11 10:05:40 +0100

    create course service squelleton

--- groupb-courses/src/app/services/course.service.ts
+++ groupb-courses/src/app/services/course.service.ts
@@ -1,9 +1,52 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  /**
+   * Get all courses
+   * @returns Observable<Course[]>
+   */
+   all() {
+  }
+
+
+  /**
+   * Get a course with the given id
+   * @param id : course id
+   * @returns Observable<Course>
+   */
+  get(id: string) {
+  }
+
+  /**
+   * Create a new course
+   * @param course new course to create
+   */
+  create(course: any) {
+
+  }
+
+  /**
+   * Update a course with the given id
+   * @param id course id to update
+   * @param course new course data
+   */
+  update(id: string, course: any) {
+  }
+
+  /**
+   * Delete a course with the given id
+   * @param id course id to delete
+   */
+  delete(id: string) {
+
+  }
 }
