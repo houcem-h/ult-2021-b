af7cb20e63a79352b29a84a7285c027c46f561cd
Houcem Hedhly <hedhoucem@gmail.com>
2021-12-11 11:03:59 +0100

    npm i -g nodemon

--- groupb-courses/backend/server.js
+++ groupb-courses/backend/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end('This is the server response !');
+    res.end('This is the new server response !');
 });
 
 server.listen(process.env.PORT || 3000);
